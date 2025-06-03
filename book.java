c8c394834b34c19f2c109ef25e51ab2a169e6ada
Tuğçe ünlü <72610188+tugceunlu@users.noreply.github.com>
2025-06-03 17:47:56 +0300

    Create book.java

---
+++ book.java
@@ -0,0 +1,35 @@
+package library;
+
+public class Book {
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true;
+    }
+
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public boolean isAvailable() { return isAvailable; }
+
+    public void borrowBook() {
+        if (isAvailable) {
+            isAvailable = false;
+            System.out.println(title + " has been borrowed.");
+        } else {
+            System.out.println(title + " is not available.");
+        }
+    }
+
+    public void returnBook() {
+        isAvailable = true;
+        System.out.println(title + " has been returned.");
+    }
+
+    public void displayInfo() {
+        System.out.println("Title: " + title + ", Author: " + author + ", Available: " + isAvailable);
+    }
+}
